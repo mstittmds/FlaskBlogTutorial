1b2318c242636b0932fadcd01e7ed7c55c200971
Michael Stitt <mdsk4v@umsystem.edu>
2024-11-01 12:54:44 -0500

    Updating the code for part 4 adding in creation of blog posts

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
+app.config['SECRET_KEY']= 'your secret key'
 
 
 # Function to open a connection to the database.db file
@@ -34,6 +34,22 @@ def index():
 
 
 # route to create a post
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
 
+        if not title:
+            flash("Title is required")
+        elif not content:
+            flash('Content is reuqired')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?,?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
 
 app.run()
